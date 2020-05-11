11bb618be582fd4c0a77f698f16d5e2f548bc8f7
rjsdn97ly <58060300+rjsdn97ly@users.noreply.github.com>
2020-05-12 06:52:13 +0900

    Week_05_Report
    Week_05_Report

---
+++ TreeEx_1.java
@@ -0,0 +1,67 @@
+
+class TreeNode{
+
+	Object data;
+	TreeNode left;
+	TreeNode right;
+	
+}
+
+class LinkedTree{
+	
+	private TreeNode root;
+	
+	public TreeNode makeBT(TreeNode bt1, Object data, TreeNode bt2) {
+		TreeNode root = new TreeNode();
+		root.data = data;
+		root.left = bt1;
+		root.right = bt2;
+		return root;
+	}
+	
+	public void preorder(TreeNode root) {
+		if(root != null) {
+			System.out.printf("%c", root.data);
+			preorder(root.left);
+			preorder(root.right);
+		}
+	}
+	
+	public void inorder(TreeNode root) {
+		if(root != null) {
+			preorder(root.left);
+			System.out.printf("%c", root.data);
+			preorder(root.right);
+		}
+	}
+	
+	public void postorder(TreeNode root) {
+		if(root != null) {
+			preorder(root.left);
+			preorder(root.right);
+			System.out.printf("%c", root.data);
+		}
+	}
+	
+}
+
+public class TreeEx_1 {
+	
+	public static void main(String args[]) {
+		LinkedTree T = new LinkedTree();
+		TreeNode n7 = T.makeBT(null, 'D', null);
+		TreeNode n6 = T.makeBT(null, 'C', null);
+		TreeNode n5 = T.makeBT(null, 'B', null);
+		TreeNode n4 = T.makeBT(null, 'A', null);
+		TreeNode n3 = T.makeBT(n6, '/', n7);
+		TreeNode n2 = T.makeBT(n4, '*', n5);
+		TreeNode n1 = T.makeBT(n2, '-', n3);
+		System.out.println("\n preorder");
+		T.preorder(n1);
+		System.out.println("\n inorder");
+		T.inorder(n1);
+		System.out.println("\n postorder");
+		T.postorder(n1);
+	}
+
+}
